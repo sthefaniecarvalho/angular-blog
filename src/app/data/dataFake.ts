58da80056757cff0ed91a986cd9b9770af14f6d7
Sthefanie <sthefaniegc06@gmail.com>
2023-10-22 12:21:23 -0300

    Project created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,31 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQ9mL0HSsG840HeAnW29MtzwpufU6Q0TYuCA&usqp=CAU",
+    "contentTitle":"Itália",
+    "contentDescription": "A Itália é um país europeu situado na porção sul do continente. Sua capital é a cidade de Roma. Banhada pelo mar Mediterrâneo, a Itália dispõe de climas mediterrâneo e temperado, o que varia conforme a altitude. O relevo do país é caracterizado por regiões planas e acidentadas, com destaque para feições como os Alpes e os Apeninos.",
+    "contentImages": [""]
+  },
+  {
+    "id":"2",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlvgUXVF1t_De6k3oKjiygXJiNKibTAbswUA&usqp=CAU",
+    "contentTitle":"Roma",
+    "contentDescription": "Roma, a capital da Itália, é uma cidade cosmopolita, enorme, com quase 3.000 anos de arte, arquitetura e cultura influentes no mundo todo e à mostra. Ruínas antigas como o Fórum e o Coliseu evocam o poder do antigo Império Romano. A Cidade do Vaticano, sede da Igreja Católica Romana, tem a Basílica de São Pedro e os museus do Vaticano, que abrigam obras-primas como os afrescos da Capela Sistina de Michelângelo.",
+    "contentImages": ["https://www.tudosobreroma.com/f/italia/roma/galeria/thumbs/foro-romano.jpg", "https://www.tudosobreroma.com/f/italia/roma/galeria/thumbs/fontana-trevi.jpg", "https://ichef.bbci.co.uk/news/640/cpsprodpb/31b4/live/46410070-c2ce-11ed-9ef5-91ab7d4e854c.jpg"]
+  },
+  {
+    "id":"3",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTntFMBA79GIsva7RCNoElOuMnH7Xs9dH_2SA&usqp=CAU",
+    "contentTitle":"Veneza",
+    "contentDescription": "Veneza, a capital da região de Vêneto, no norte da Itália, é formada por mais de 100 pequenas ilhas em uma lagoa no Mar Adriático. A cidade não tem estradas, apenas canais (como a via Grand Canal), repletos de palácios góticos e renascentistas. Na praça central, Piazza San Marco, ficam a Basílica de São Marcos, coberta de mosaicos bizantinos, e o campanário, com vista para os telhados vermelhos da cidade."
+  },
+  {
+  "id":"4",
+    "photoCover":"https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcTa0FnJCpCs9COT1XERLfovORqO4ZsmVP8zxHMrwRi3MSfFCzpmRRJZAdLdKujdB_uO",
+    "contentTitle":"Milão",
+    "contentDescription": "Milão, uma metrópole na região da Lombardia ao norte da Itália, é uma capital mundial da moda e do design. Cidade da bolsa de valores nacional, trata-se de um centro financeiro também conhecido pelas suas lojas e pelos seus restaurantes sofisticados. A catedral Gothic Duomo di Milano e o convento Santa Maria delle Grazie, onde está o mural de Leonardo da Vinci 'A Última Ceia', são testemunhas de séculos de arte e cultura."
+  },
+
+]
+
+
